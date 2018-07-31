f097bebb2f3a3bb1428d56bf8dfb8144ba7c1f6d
liquidsword <audenrobertson@gmail.com>
2018-07-30 20:07:15 -0400

    updated license

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180730222448) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "artist_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "drawings", force: :cascade do |t|
+    t.string   "art"
+    t.integer  "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
